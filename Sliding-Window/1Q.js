9df4b6204bfdc4448eec08e02476952762bde17f
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-06-19 14:43:07 +0530

    commit

--- Sliding-Window/1Q.js
+++ Sliding-Window/1Q.js
@@ -15,7 +15,7 @@ const MaxSumSubarrayofsizeK = (arr, k) => {
     return maxSum
 }
 
-console.log(MaxSumSubarrayofsizeK([2, 1, 5, 1, 3, 2], 3));
+// console.log(MaxSumSubarrayofsizeK([2, 1, 5, 1, 3, 2], 3));
 
 
 
@@ -47,4 +47,57 @@ const minSubArrayLen = (nums, target) => {
     return res === Infinity ? 0 : res;
 }
 
-console.log(minSubArrayLen([2,3,1,2,4,3], 7));
+// console.log(minSubArrayLen([2,3,1,2,4,3], 7));
+
+// ============================================================
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+const MaximumAverageSubarrayI = (nums, k) => {
+    let windowSum = 0;
+
+    for (let i = 0; i < k; i++){
+        windowSum = (windowSum + nums[i]) 
+    }
+
+    let maxSum = windowSum / k;
+    
+
+
+    for(let j = k ; j < nums.length ; j++){
+        windowSum = windowSum - nums[j - k] + nums[j];
+        console.log('windowSum', windowSum);
+        
+        maxSum = Math.max(maxSum, windowSum / k)
+        
+    }
+    return maxSum
+}
+
+// console.log(MaximumAverageSubarrayI([1,12,-5,-6,50,3], 4));
+// console.log(MaximumAverageSubarrayI([0,1,1,3,3], 4));
+
+function build(str) {
+    const stack = [];
+
+    for (const ch of str) {
+        if (ch === "#") {
+            stack.pop();
+        } else {
+            stack.push(ch);
+        }
+    }
+
+    return stack.join("");
+}
+var backspaceCompare = function(s, t) {
+    return build(s) === build(t);
+};
+
+
+// console.log(backspaceCompare("ab#c", "ad#c"));
+// console.log(backspaceCompare("ab##", "c#d#"));
+// console.log(backspaceCompare("a#c", "b"));
+
+
